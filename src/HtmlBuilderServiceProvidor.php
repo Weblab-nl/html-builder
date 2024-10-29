034e0807b57c5f99176396a93af76187b200538f
hasan <hasan@weblab.nl>
2024-10-29 16:27:35 +0100

    feature/WEB-10414 - added stuuf to composer.json

---
+++ src/HtmlBuilderServiceProvidor.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Weblab\html\HtmlBuilder;
+
+use Illuminate\Support\ServiceProvider;
+
+class HtmlBuilderServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        dd('test');
+    }
+}
